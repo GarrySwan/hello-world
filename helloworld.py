2fca88652e47751777b94b3e8ef991ae730469de
Gazza <garry.swan@csiro.au>
2016-06-18 02:19:11 +0000

    for loop added

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,5 @@ print('hello world')
 print('New Line added and commited')
 print('and here is another line')
 print('and here is another line and another')
+for letter in 'Hello World':
+    print("Letters: ", letter)
