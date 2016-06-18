a111ff73536d1efdb06edff77f6232e16bc88d8d
Gazza <garry.swan@csiro.au>
2016-06-18 11:43:16 +0000

    Sense stuff

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+
+
 print('hello world')
 print('New Line added and commited')
 print('and here is another line')
---
+++ sensehattest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import time
+from sense_hat import SenseHat
+
+
+sense = SenseHat()
+
+sense.clear(0,255,255)
+
+
+
+
